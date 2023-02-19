8b4714b15fce100ef33f5d8e977a88627c3aadf4
AlessandroNodari <102602287+AlessandroNodari@users.noreply.github.com>
2023-02-19 17:16:30 +0100

    Update variance_curve.py

--- Calibration/variance_curve.py
+++ Calibration/variance_curve.py
@@ -2,8 +2,11 @@ import numpy as np
 
 # Compute the initial forward variance curve at a given time t
 # using the Gompertz function with precomputed parameters
-def variance_curve(t):
+def g(t):
     z1 = 0.23934445541427635
     z2 = 0.23559167522825925
     z3 = 0.19271882494190468
     return z1 * np.exp(-z2 * np.exp(-z3 * t))
+    
+def variance_curve(t):
+    return g(t)**2 + t*(g(t+eps)**2 - g(t-eps)**2)/(2*eps)
