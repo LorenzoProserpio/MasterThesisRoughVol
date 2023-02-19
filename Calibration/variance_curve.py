e7f0d89ddee348360171ccaf685d8414bd32bbc1
AlessandroNodari <102602287+AlessandroNodari@users.noreply.github.com>
2023-02-19 17:34:19 +0100

    Update variance_curve.py

--- Calibration/variance_curve.py
+++ Calibration/variance_curve.py
@@ -9,5 +9,7 @@ def g(t):
     return z1 * np.exp(-z2 * np.exp(-z3 * t))
     
 def variance_curve(t):
-    eps = 1e-8
-    return g(t)**2 + t*(g(t+eps)**2 - g(t-eps)**2)/(2*eps)
+    z1 = 0.23934445541427635
+    z2 = 0.23559167522825925
+    z3 = 0.19271882494190468
+    return g(t)**2 + 2*t*z1**2*z2*z3*np.exp(-2*z2*np.exp(-z3*t)-z3*t)
