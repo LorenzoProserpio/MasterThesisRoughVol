0d2be5664ab03034351473cf618ca53083aeb730
AlessandroNodari <102602287+AlessandroNodari@users.noreply.github.com>
2023-05-11 12:47:28 +0200

    Add files via upload

---
+++ Calibration/Bayesian Quintic OU/variance_curve.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+# Compute the initial forward variance curve at a given time t
+# using the Gompertz function with precomputed parameters
+def g(t):
+    z1 = 0.23934445541427635
+    z2 = 0.23559167522825925
+    z3 = 0.19271882494190468
+    return z1 * np.exp(-z2 * np.exp(-z3 * t))
+    
+def variance_curve(t):
+    z1 = 0.23934445541427635
+    z2 = 0.23559167522825925
+    z3 = 0.19271882494190468
+    t = t * 12
+    return g(t)**2 + 2*t*z1**2*z2*z3*np.exp(-2*z2*np.exp(-z3*t)-z3*t)
