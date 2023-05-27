335574f55a5a2290fae74e76f5ee64c6f1ef6606
LorenzoProserpio <91561378+LorenzoProserpio@users.noreply.github.com>
2023-05-27 23:58:43 +0200

    Add files via upload

--- Calibration/ImpliedDrift.py
+++ Calibration/ImpliedDrift.py
@@ -14,7 +14,7 @@ def r(x, index = 0):
     rates = np.array(data[dates[index]])/100
     cs = CubicSpline(tenor, rates)
 	if(x>20):
-		return cs(20)
+            return cs(20)
     return cs(x)
     
 def drift(x, index = 0):
@@ -22,7 +22,7 @@ def drift(x, index = 0):
     d = -np.log(S0/F).flatten()/TENOR
 	cs = CubicSpline(TENOR, d)
 	if(x>20):
-		return cs(20)
+            return cs(20)
     return cs(x)
     
 def q(x, index = 0):
