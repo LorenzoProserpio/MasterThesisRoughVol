1cda36682c77ecf37b9c092f57ea62d8f39bf190
AlessandroNodari <102602287+AlessandroNodari@users.noreply.github.com>
2023-06-03 19:44:24 +0200

    Add files via upload

--- Calibration/Bayesian rBergomi/ImpliedDrift.py
+++ Calibration/Bayesian rBergomi/ImpliedDrift.py
@@ -1,15 +1,25 @@
 import numpy as np
+import pandas as pd
+from scipy.interpolate import CubicSpline
 
-def implied_drift(tenor):
-    '''Calculate the drift term for different tenor (in years)'''
+dates = np.array(["23-01-23","24-01-23","25-01-23","26-01-23","27-01-23","30-01-23","06-02-23","13-02-23","21-02-23"])
+data = pd.read_csv("ratesOIS.csv")
+tenor = np.array(data.TENOR)
+F = np.array(pd.read_csv("implied_forward.csv"))
+spot = np.array(pd.read_csv("spot.csv").Spot)
+TENOR = np.array(pd.read_csv("hist_spx.csv")["Exp Date"])
+
+
+def r(x, index = 0):
+    rates = np.array(data[dates[index]])/100
+    cs = CubicSpline(tenor, rates)
+    return cs(x)
     
-    a,b,c,d = (0.13150334, 1.67001571, 9.41005645, 0.02267608)
-    T_max = 3629.999999835/365.
+def drift(x, index = 0):
+    S0 = spot[index]
+    d = -np.log(S0/F).flatten()/TENOR
+    cs = CubicSpline(TENOR, d)
+    return cs(x)
     
-    if tenor > 30/365. and tenor < T_max:
-        return (a*(tenor/T_max)**(b-1)*(1-tenor/T_max)**(c-1))+d
-    if tenor >= T_max:
-        return d
-    if tenor <= 30/365. and tenor >= 13.9/365.:
-        return np.interp(tenor*365,[14,30.],[0.03449854534679646,0.027609263139159457])
-    print('Error: cannot accept tenors under 14 days.')
+def q(x, index = 0):
+    return r(x, index) - drift(x, index)
