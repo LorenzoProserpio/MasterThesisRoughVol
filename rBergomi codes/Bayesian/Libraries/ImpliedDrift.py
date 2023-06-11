26e6de25ec12e4b2745475d7956ad334ad0df0d9
AlessandroNodari <102602287+AlessandroNodari@users.noreply.github.com>
2023-06-11 18:34:55 +0200

    Add files via upload

---
+++ rBergomi codes/Bayesian/Libraries/ImpliedDrift.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from scipy.interpolate import CubicSpline
+
+dates = np.array(["23-01-23","24-01-23","25-01-23","26-01-23","27-01-23","30-01-23","06-02-23","13-02-23","21-02-23"])
+data = pd.read_csv("ratesOIS.csv")
+tenor = np.array(data.TENOR)
+F = np.array(pd.read_csv("implied_forward.csv"))
+spot = np.array(pd.read_csv("spot.csv").Spot)
+TENOR = np.array(pd.read_csv("hist_spx.csv")["Exp Date"])
+
+
+def r(x, index = 0):
+    rates = np.array(data[dates[index]])/100
+    cs = CubicSpline(tenor, rates)
+    return cs(x)
+    
+def drift(x, index = 0):
+    S0 = spot[index]
+    d = -np.log(S0/F).flatten()/TENOR
+    cs = CubicSpline(TENOR, d)
+    return cs(x)
+    
+def q(x, index = 0):
+    return r(x, index) - drift(x, index)
